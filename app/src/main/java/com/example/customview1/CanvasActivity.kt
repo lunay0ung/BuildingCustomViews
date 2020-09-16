51178d5d3ec75f848cc3d00679c7e885d880da02
Soyoung Yang <sysy@snowcorp.com>
2020-09-16 16:42:49 +0900

    드로잉을 위한 커스텀 뷰 생성

---
+++ app/src/main/java/com/example/customview1/CanvasActivity.kt
@@ -0,0 +1,20 @@
+package com.example.customview1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
+
+class CanvasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // 커스텀뷰를 코드 상으로 생성시키므 xml 파일은 필요 없다.
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
+        //드로잉을 위해서는 뷰의 사이즈를 알아야 하지만 onCreate()에서는 알 수 없다. 이 시점에서 아직 사이즈는 결정되지 않았기 때문이다.
+
+
+    }
+}
---
+++ app/src/main/java/com/example/customview1/MyCanvasView.kt
@@ -0,0 +1,9 @@
+package com.example.customview1
+
+import android.content.Context
+import android.view.View
+
+class MyCanvasView(context: Context) : View(context) {
+
+
+}
